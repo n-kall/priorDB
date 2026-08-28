c43141088baf109bea31fc4e62f2c83aa91eb79d
aloctavodia <aloctavodia@gmail.com>
2026-08-28 16:08:59 +0300

    alt

--- pymc/r2d2.py
+++ pymc/r2d2.py
@@ -2,9 +2,11 @@ import numpy as np
 import pymc as pm
 import pytensor.tensor as pt
 
-# center predictors and scale priors with the outcome
+# center and scale predictors
 # Assume X is your predictor matrix and y is your outcome vector
-Xc = X - X.mean(axis=0)
+X_mean = X.mean(axis=0)
+X_sd = X.std(axis=0, ddof=1)
+Xc = (X - X_mean) / X_sd
 sd_y = y.std(ddof=1)
 
 # HyperParameters for the R2D2 prior
@@ -18,7 +20,7 @@ with pm.Model() as model:
     # prior for the intercept and error term
     # not part of the R2D2 prior, but needed for the model
     sigma = pm.HalfStudentT("sigma", nu=3, sigma=sd_y)
-    a_c = pm.StudentT("a_c", nu=4, mu=0.0, sigma=sd_y)
+    a_c = pm.StudentT("a_c", nu=4, mu=y.mean(), sigma=sd_y)
 
     # R2D2 prior
     zb = pm.Normal("zb", mu=0.0, sigma=1.0, shape=K)
