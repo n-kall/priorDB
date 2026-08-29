5cdf845c7a36291e4656f93398e7546d49144f9b
aloctavodia <aloctavodia@gmail.com>
2026-08-29 10:52:25 +0300

    expand grd2 entry and add code

--- pymc/gr2.py
+++ pymc/gr2.py
@@ -9,27 +9,31 @@ X_sd = X.std(axis=0, ddof=1)
 Xc = (X - X_mean) / X_sd
 sd_y = y.std(ddof=1)
 
-# HyperParameters for the R2D2 prior
+# HyperParameters for the GDR2 prior
 # Adjust as needed for your specific problem
 K = X.shape[1]
-cons_D2 = np.full(K, 1)
-mean_R2 = 0.3
-prec_R2 = 3.0
+mu_phi = np.zeros(K - 1)
+mean_R2 = 0.5
+prec_R2 = 2
+
+
 
 with pm.Model() as model:
     # prior for the intercept and error term
-    # not part of the R2D2 prior, but needed for the model
+    # not part of the GDR2 prior, but needed for the model
     sigma = pm.HalfStudentT("sigma", nu=3, sigma=sd_y)
     a_c = pm.StudentT("a_c", nu=4, mu=y.mean(), sigma=sd_y)
 
-    # R2D2 prior
+    # priors
     zb = pm.Normal("zb", mu=0.0, sigma=1.0, shape=K)
-    phi = pm.Dirichlet("phi", a=cons_D2)
+    eta = pm.Normal("eta", mu=mu_phi, sigma=np.pi, shape=K - 1)
+    eta_full = pt.concatenate([eta, pt.zeros(1)])
+    phi = pm.Deterministic("phi", pm.math.softmax(eta_full))
     R2 = pm.Beta("R2", mu=mean_R2, nu=prec_R2)
 
-    # compute coefficients following the R2D2 prior
-    tau2 = sigma**2 * R2 / (1 - R2)
-    b = pm.Deterministic("b", zb * pt.sqrt(phi * tau2))
+    # compute coefficients following the GDR2 prior
+    tau2 = R2 / (1 - R2)
+    b = pm.Deterministic("b", zb * sigma * pt.sqrt(phi * tau2))
 
     mu = a_c + Xc @ b
     pm.Normal("y_obs", mu=mu, sigma=sigma, observed=y)
